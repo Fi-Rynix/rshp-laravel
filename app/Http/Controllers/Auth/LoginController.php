becc66e49ef930adefbbe57bea7c7535a8cf2333
Fi-Rynix <rafiihyaazzaky@gmail.com>
2025-10-27 11:52:06 +0700

    Modul 10 (Login controller)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,64 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('Auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $validator = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $user = User::with(['roleUsers' => function($query) {
+            $query->where('status', 1);
+        }, 'roleUsers.role'])->where('email', $request->email)->first();
+
+        if (!$user) {
+            return redirect()->back()->withErrors(['email' => 'email tidak ditemukan'])->withInput();
+        }
+
+        if (!\Hash::check($request->password, $user->password)) {
+            return redirect()->back()->withErrors(['password' => 'password salah'])->withInput();
+        }
+
+        $nama_role = Role::where('idrole', $user->roleUsers[0]->idrole ?? null)->first();
+
+        Auth::login($user);
+
+        $request->session()->put([
+            'iduser' => $user->iduser,
+            'nama' => $user->nama,
+            'email' => $user->email,
+            'idrole' => $user->roleUsers[0]->idrole ?? 'User',
+            'role' => $nama_role->nama_role ?? 'User',
+            'status' => $user->roleUsers[0]->status ?? '0',
+        ]);
+
+        $user_role = $user->roleUser[0]->idrole ?? null;
+
+        switch ($user_role) {
+            case 1:
+                return redirect()->route('Admin.dashboard')->with('success', 'Login berhasil');
+            case 2:
+                return redirect()->route('Dokter.dashboard')->with('success', 'Login berhasil');
+            case 3:
+                return redirect()->route('Perawat.dashboard')->with('success', 'Login berhasil');
+            case 4:
+                return redirect()->route('Resepsionis.dashboard')->with('success', 'Login berhasil');
+            default:
+                return redirect()->route('Auth.login')->withErrors(['role' => 'Role tidak dikenali'])->withInput(request()->all());
+        }
     }
 }
